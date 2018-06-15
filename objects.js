9ffad4b726717a373a494bb19739f93c04ef6539
Andrew Sommer <sommer.drew@gmail.com>
2018-06-15 13:47:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,12 +8,12 @@
 meals.snack = 'yogurt';
 */
 
-function updatePlayList(obj, strArtist, strSongTitle) {
+function updatePlaylist(obj, strArtist, strSongTitle) {
   obj[strArtist]=strSongTitle;
   return obj;
 }
 
-function removeFromPlayList(obj, strArtist) {
+function removeFromPlaylist(obj, strArtist) {
   delete obj[strArtist];
   return obj;
 }
@@ -22,8 +22,12 @@ var playlist = {};
 playlist.Prince='1999';
 playlist['Grateful Dead']='Casey Jones';
 console.log(JSON.stringify(playlist));
-console.log(JSON.stringify(updatePlayList(playlist, 'Bob Dylan','Like a Rolling Stone')));
+console.log(JSON.stringify(updatePlaylist(playlist, 'Bob Dylan','Like a Rolling Stone')));
 console.log(JSON.stringify(playlist));
+
+console.log(JSON.stringify(removeFromPlaylist(playlist, 'Bob Dylan')));
+console.log(JSON.stringify(playlist));
+
 
 //var returnString =
 //console.log(returnString);
